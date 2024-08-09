4b69ae11fde0565ea1d22683d3e2dcd070b31fe2
FreelancerMamun <mamunkhan16345@gmail.com>
2024-08-09 16:26:11 +0600

    add layout

---
+++ src/pages/books.tsx
@@ -0,0 +1,11 @@
+const Books = () => {
+  return (
+    <div>
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Books</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,38 @@
 import { createBrowserRouter } from 'react-router-dom';
-
 import { LoginPage } from './pages/login';
 import { HomePage } from './pages/Home';
 import Register from './pages/register';
+import { DashboardLayout } from './layout/DashboardLayout';
+import Books from './pages/books';
+import AuthLayout from './layout/AuthLayout';
 export const router = createBrowserRouter([
   {
-    path: '/login',
-    element: <LoginPage />,
+    path: '/auth',
+    element: <AuthLayout />,
+
+    children: [
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+    ],
   },
   {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
+    path: '/dashboard',
+    children: [
+      {
+        path: 'home',
+        element: <HomePage></HomePage>,
+      },
+      {
+        path: 'books',
+        element: <Books></Books>,
+      },
+    ],
+    element: <DashboardLayout />,
   },
 ]);
